e5debdd8a58e71ac3637ec5fba0928a8f47f2e32
Desil Vared <arskang@gmail.com>
2018-04-24 23:34:50 -0500

    Commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,39 @@
+// requireds
+const fs = require('fs');
+const colors = require('colors');
+
+let listarTabla = (base, limite = 10) => {
+
+    console.log("------------------------------------".red);
+    console.log(`--- Tabla de multiplicar del ${base}`.red.bold);
+    console.log("------------------------------------".red);
+
+    for (let i = 1; i <= limite; i++) {
+        console.log(`${ base } * ${ i } = ${ base * i }`);
+    }
+
+}
+
+let crearArchivo = (base, limite = 10) => {
+    return new Promise((resolve, reject) => {
+
+        if (!Number(base)) {
+            reject(`El valor introducido '${base}' no es un número`);
+            return;
+        }
+        let data = '';
+
+        for (let i = 1; i <= limite; i++) {
+            data += `${ base } * ${ i } = ${ base * i }\n`;
+        }
+
+        fs.writeFile(`tablas/tabla-${base}-al-${limite}.txt`, data, (err) => {
+            if (err) { reject(err); } else { resolve(`tabla-${base}-al-${limite}.txt`.green.bold); }
+        });
+    })
+}
+
+module.exports = {
+    crearArchivo,
+    listarTabla
+}
